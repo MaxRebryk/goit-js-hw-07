1dff34c4ef2e4f6be3542b218e38bd1c5fd8bb6e
Max Rebryk <rebryk2003@gmail.com>
2024-02-11 22:09:19 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,14 @@
 "use strict";
 
+const categoryUl = document.querySelector("#categories");
+const categoryAllLi = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoryUl.children.length}`);
+
+categoryAllLi.forEach(categoryItem => {
+  const categoryName = categoryItem.querySelector("h2").textContent;
+  const categoryItemsElements = categoryItem.querySelectorAll('li').length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Number of items: ${categoryItemsElements}`);
+},0);
+
